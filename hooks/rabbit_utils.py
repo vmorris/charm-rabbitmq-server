582cef9f61e4211e6a00c1bae754f4c8e94ab7c1
yolanda.robla@canonical.com <>
2014-01-09 11:07:19 +0100

    typo

--- hooks/rabbit_utils.py
+++ hooks/rabbit_utils.py
@@ -226,7 +226,7 @@ def synchronize_service_credentials():
     '''
     if not os.path.isdir(LIB_PATH):
         return
-    peers = cluster.peers_units()
+    peers = cluster.peer_units()
     if peers and not cluster.oldest_peers(peers):
         utils.juju_log('INFO', 'Deferring action to oldest service unit.')
 
