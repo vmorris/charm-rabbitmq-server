68ae8690b7b9d6b8b09837cd55e53449ab82aa4b
yolanda.robla@canonical.com <>
2014-01-28 18:15:21 +0100

    typo in ha queues

--- hooks/rabbit_utils.py
+++ hooks/rabbit_utils.py
@@ -103,8 +103,7 @@ def cluster_with():
     vers = rabbit_version()
     if vers >= '3.0.1-1':
         cluster_cmd = 'join_cluster'
-        cmd = [RABBITMQ_CTL, 'set_policy HA \'^(?!amq\.).*\' '
-               '\'{"ha-mode": "all"}\'']
+        cmd = ['/usr/sbin/rabbitmqctl', 'set_policy', 'HA', '^(?!amq\\.).*', '{"ha-mode": "all"}']
         subprocess.check_call(cmd)
     else:
         cluster_cmd = 'cluster'
