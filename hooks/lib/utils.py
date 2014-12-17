98b530e7296173b74152387c83f0c538c28fa37c
Corey Bryant <corey.bryant@canonical.com>
2014-12-17 18:07:58 +0000

    Drop render_template() and use charm-helpers render() instead. Same functionality, just simpler code.

--- hooks/lib/utils.py
+++ hooks/lib/utils.py
@@ -21,20 +21,6 @@ from charmhelpers.core.hookenv import (
     remote_unit,
     log
 )
-TEMPLATES_DIR = 'templates'
-
-try:
-    import jinja2
-except ImportError:
-    apt_install('python-jinja2', fatal=True)
-    import jinja2
-
-
-def render_template(template_name, context, template_dir=TEMPLATES_DIR):
-    templates = \
-        jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
-    template = templates.get_template(template_name)
-    return template.render(context)
 
 
 def is_newer():
