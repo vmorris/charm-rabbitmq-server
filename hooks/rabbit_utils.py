daa4214acf1d967bf0bb58f6a79e21bad58369da
Ryan Beisner <ryan.beisner@canonical.com>
2016-09-08 22:09:52 +0000

    Revert changes relating to management of /etc/hosts
    Switching to active management by the charm of /etc/hosts
    resulted in a number of race conditions during cluster
    formation as a nasty side effect.
    
    Revert changes to the charm related to this switch, as charm
    versions prior to these changes are far more reliable to
    deploy.
    
    Further work is required to support MAAS 2.0 with Juju 2.0.
    
    This reverts commit 0ded449e8d32428baf7992cfc6999f82e9996c1a.
    This reverts commit 7ffdcd204e6550cf5dcad57ed07ea302c1eedcce.
    
    Closes-Bug: 1592200
    
    Change-Id: Ib2d6714ac1c4b32542cea02c24f75d7ffbde8282
    (cherry picked from commit af00c66eba0f94ebd40f4287785a26a5a0ec469b)

--- hooks/rabbit_utils.py
+++ hooks/rabbit_utils.py
@@ -20,6 +20,7 @@ import glob
 import tempfile
 import random
 import time
+import socket
 
 from rabbitmq_context import (
     RabbitMQSSLContext,
@@ -30,6 +31,8 @@ from charmhelpers.core.templating import render
 
 from charmhelpers.contrib.openstack.utils import (
     _determine_os_workload_status,
+    get_hostname,
+    get_host_ip,
     pause_unit,
     resume_unit,
     is_unit_paused_set,
@@ -43,6 +46,7 @@ from charmhelpers.core.hookenv import (
     service_name,
     status_set,
     cached,
+    unit_get,
     relation_set,
 )
 
@@ -60,8 +64,6 @@ from charmhelpers.contrib.peerstorage import (
     peer_store,
     peer_retrieve
 )
-
-from socket import gethostname as get_local_nodename
 
 from collections import OrderedDict
 
@@ -345,7 +347,7 @@ def wait_app():
 
 def start_app():
     ''' Start the rabbitmq app and wait until it is fully started '''
-    status_set('maintenance', 'Starting RabbitMQ')
+    status_set('maintenance', 'Starting rabbitmq applilcation')
     rabbitmqctl('start_app')
     wait_app()
 
@@ -667,11 +669,32 @@ def leader_node():
     ''' Provide the leader node for clustering '''
     # Each rabbitmq node should join_cluster with the leader
     # to avoid split-brain clusters.
-    leader_nodename = peer_retrieve('leader_nodename')
-    if leader_nodename:
-        return "rabbit@{}".format(leader_nodename)
-    else:
-        return None
+    leader_node_ip = peer_retrieve('leader_node_ip')
+    if leader_node_ip:
+        return "rabbit@" + get_node_hostname(leader_node_ip)
+
+
+def get_node_hostname(ip_addr):
+    ''' Resolve IP address to hostname '''
+    try:
+        nodename = get_hostname(ip_addr, fqdn=False)
+    except:
+        log('Cannot resolve hostname for %s using DNS servers' % ip_addr,
+            level='WARNING')
+        log('Falling back to use socket.gethostname()',
+            level='WARNING')
+        # If the private-address is not resolvable using DNS
+        # then use the current hostname
+        nodename = socket.gethostname()
+    log('local nodename: %s' % nodename, level=INFO)
+    return nodename
+
+
+def get_local_nodename():
+    '''Resolve local nodename into something that's universally addressable'''
+    ip_addr = get_host_ip(unit_get('private-address'))
+    log('getting local nodename for ip address: %s' % ip_addr, level=INFO)
+    return get_node_hostname(ip_addr)
 
 
 @cached
